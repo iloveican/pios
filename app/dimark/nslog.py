721a44fc615a6547d1b2f5fa424c6d989aa25b3d
Dima Tisnek <dimaqq@gmail.com>
2017-02-09 13:52:42 +0100

    Access NSLog

---
+++ app/dimark/nslog.py
@@ -0,0 +1,29 @@
+import logging
+
+from rubicon.objc import objc_method, ObjCClass, ObjCInstance, send_message
+logging.debug("yippie, loaded objc")
+
+class NSString(ObjCInstance):
+    @classmethod
+    def from_python(cls, s):
+        b = s.encode('utf8')
+        obj = send_message(ObjCClass("NSString"), b'stringWithUTF8String:', b)
+        return cls(obj)
+
+    def __str__(self):
+        s = ctypes.string_at(self.send(b'UTF8String', raw=True))
+        return s.decode('utf8')
+
+import ctypes
+import ctypes.util
+extension = ctypes.cdll.LoadLibrary(ctypes.util.find_library("extension"))
+extension.NSLog.restype = None
+extension.NSLog.argtypes = (ctypes.c_void_p, )
+
+
+def NSLog(s):
+    extension.NSLog(NSString.from_python(s))
+
+
+for i in range(10000):
+    NSLog("foobarbazgoogoooooo\N{EURO SIGN} %s" % i)
