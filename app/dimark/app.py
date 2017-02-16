2745c92e0266342d499da9b1d2774d37f2a65d55
Dima Tisnek <dimaqq@gmail.com>
2017-02-16 18:23:40 +0100

    Logging work mid-way

--- app/dimark/app.py
+++ app/dimark/app.py
@@ -1,32 +1,68 @@
 import logging
 import bootstrap
 
-try:
-    from rubicon.objc import objc_method
-    logging.debug("yippie, loaded objc")
-except (Exception, BaseException):
-    logging.exception("oh no")
+from rubicon.objc import objc_method, ObjCClass
+logging.debug("yippie, loaded objc")
 
 try:
-    # class PythonAppDelegate():
-  class xxPythonAppDelegate(UIResponder):
+    import ctypes
+    import ctypes.util
+    extension = ctypes.cdll.LoadLibrary(ctypes.util.find_library("extension"))
+    NSLog = extension.NSLog
+    NSLog.restype = None
+    NSLog.argtypes = (...)
+
+    # autorelease block
+    # [NSString stringWithUTF8String:name]; <-- s.encode("utf-8")
+    # NSLog(that)
+    NSLog("test")
+except:
+    logging.exception("Setting up real logging")
+
+class PythonAppDelegate(ObjCClass('UIResponder')):
     def __init__(self):
-        print("starting")
-        logging.warn("hahaha")
+        logging.debug("instance created")
 
     @objc_method
     def applicationDidBecomeActive(self) -> None:
-        print("became active")
+        logging.debug("became active")
 
     @objc_method
     def application_didFinishLaunchingWithOptions_(self, application, oldStatusBarOrientation: int) -> None:
-        print("finished launching", application, oldStatusBarOrientation)
+        logging.debug("finished launching %s %s", application, oldStatusBarOrientation)
+        try:
+            root = ObjCClass("UIViewController").alloc().init()
+            nav = ObjCClass("UINavigationController").alloc().initWithRootViewController(root)
+        except:
+            logging.exception("terrible")
 
 
     @objc_method
     def application_didChangeStatusBarOrientation_(self, application, oldStatusBarOrientation: int) -> None:
-        print("or ch", application, oldStatusBarOrientation)
-except Exception:
-    pass
-except BaseException:
-    pass
+        logging.debug("or ch %s %s", application, oldStatusBarOrientation)
+
+logging.debug("yippie, %s defined", PythonAppDelegate)
+
+
+""" Code example from Ruby:
+
+class AppDelegate
+    def application(application, didFinishLaunchingWithOptions:launchOptions)
+        rootViewController = UIViewController.alloc.init
+        rootViewController.title = 'Hello'
+        rootViewController.view.backgroundColor = UIColor.whiteColor
+
+        navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+
+        @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+        @window.rootViewController = navigationController
+        @window.makeKeyAndVisible
+
+        alert = UIAlertView.new
+        alert.message = "Hello iOS!"
+        alert.show
+
+        true
+    end
+end
+"""
