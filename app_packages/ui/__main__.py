e208471b095063d57e547671a0d72fa9c383e09c
Dima Tisnek <dimaqq@gmail.com>
2017-02-17 20:41:07 +0100

    Flow layout created

--- app_packages/ui/__main__.py
+++ app_packages/ui/__main__.py
@@ -2,12 +2,36 @@ import nslog
 import logging
 
 from rubicon.objc import objc_method, ObjCClass
+from rubicon.objc.types import CGSize
 
 
 if __name__.split(".")[-1] == "__main__":
     # I'm ran as a module
     logging.root.addHandler(nslog.handler())
     logging.debug("yep, in main")
+
+
+# class Knob(ObjCClass("UICollectionViewCell")):
+#     @objc_method
+#     # def collectionView(self, cellForItemAtIndexPath: indexPath):
+#     def collectionView(self, indexPath):
+#         logging.debug("collection view %s", [self, indexPath])
+# 
+# 
+# class Knobs(ObjCClass("UICollectionView")):
+#     @objc_method
+#     def numberOfSections(self) -> int:
+#         return 3
+# 
+#     @objc_method
+#     def collectionView(self, section: int) -> int:
+#         return 4
+# 
+#     @objc_method
+#     # def collectionViewWithReuseIdentifier(self, index_path) -> ObjCClass("UICollectionViewCell"):
+#     def collectionViewWithReuseIdentifier(self, index_path):
+#         logging.debug("viewee used")
+#         self.dequeueReusableCell("thumb", indexPath)
 
 
 class PythonAppDelegate(ObjCClass('UIResponder')):
@@ -28,8 +52,18 @@ class PythonAppDelegate(ObjCClass('UIResponder')):
             root.view.backgroundColor = ObjCClass("UIColor").blueColor()
             # root.view.backgroundColor = ObjCClass("UIColor").alloc().initWithWhite_alpha_(0.5, 0.5)
             nav = ObjCClass("UINavigationController").alloc().initWithRootViewController_(root)
+
+            win = ObjCClass("UIWindow").alloc().initWithFrame_(ObjCClass("UIScreen").mainScreen().bounds)
+            win.rootViewController = nav
+            win.makeKeyAndVisible()
         except:
             logging.exception("terrible")
+
+        try:
+            lay = ObjCClass("UICollectionViewFlowLayout").new()
+            lay.itemSize = CGSize(300, 300)
+        except:
+            logging.exception("just bad")
 
     @objc_method
     def application_didChangeStatusBarOrientation_(self, application, oldStatusBarOrientation: int) -> None:
@@ -39,24 +73,7 @@ logging.debug("yippie, %s defined", PythonAppDelegate)
 
 
 """ Code example from Ruby:
-
-class AppDelegate
-    def application(application, didFinishLaunchingWithOptions:launchOptions)
-        rootViewController = UIViewController.alloc.init
-        rootViewController.title = 'Hello'
-        rootViewController.view.backgroundColor = UIColor.whiteColor
-
-        navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
-
-        @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-        @window.rootViewController = navigationController
-        @window.makeKeyAndVisible
-
         alert = UIAlertView.new
         alert.message = "Hello iOS!"
         alert.show
-
-        true
-    end
-end
 """
