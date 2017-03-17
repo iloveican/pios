4813ab40b3461effab7ffa5c2a295476b493189a
Dima Tisnek <dimaqq@gmail.com>
2017-03-17 09:10:19 +0100

    Words are very unnecessary...

--- app_packages/game/__init__.py
+++ app_packages/game/__init__.py
@@ -3,13 +3,17 @@ import random
 
 numbers = [l.split() for l in """
 一 二 三 四 五 六 七 八 九 十
-① ② ③ ④ ⑤ ⑥ ⑦ ⑧ ⑨ ⑩ 
+① ② ③ ④ ⑤ ⑥ ⑦ ⑧ ⑨ ⑩
 """.strip().splitlines()]
 
 
+operators = "eq ne ge le gt lt pw ls tu st".split(), "== != => <= > < ** [] () {}".split()
+
+
 def get_tiles(size):
-    assert not self.size % 2
-    game = random.sample(list(range(len(numbers[0]))), size // 2)
-    tiles = sum(([(i, numbers[0][i]), (i, numbers[1][i])] for i in game), [])
+    assert not size % 2
+    source = random.choice((numbers, operators))
+    game = random.sample(list(range(len(source[0]))), size // 2)
+    tiles = sum(([(i, source[0][i]), (i, source[1][i])] for i in game), [])
     random.shuffle(tiles)
     return tiles
