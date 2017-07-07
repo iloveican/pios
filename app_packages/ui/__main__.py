13b50e6b601e6a1b51629549148cf5914d9444b0
Dima Tisnek <dimaqq@gmail.com>
2017-07-07 16:34:00 +0200

    Merge improvements from exercise 0

--- app_packages/ui/__main__.py
+++ app_packages/ui/__main__.py
@@ -11,7 +11,10 @@ import sound
 
 if __name__.split(".")[-1] == "__main__":
     # I'm ran as a module
-    logging.root.addHandler(nslog.handler())
+    # drop low-level logging handler
+    # set up logging properly
+    del logging.root.handlers[:]
+    logging.basicConfig(level=logging.DEBUG, handlers=[nslog.handler()])
     logging.debug("yep, in main")
 
 UIView = ObjCClass("UIView")
