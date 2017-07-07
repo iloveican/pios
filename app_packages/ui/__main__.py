a268d61bf6216871254bd37dc82a47c65f2c07f5
Dima Tisnek <dimaqq@gmail.com>
2017-07-07 15:38:33 +0200

    Checkpoint

--- app_packages/ui/__main__.py
+++ app_packages/ui/__main__.py
@@ -2,17 +2,12 @@ import nslog
 import logging
 
 
-# 1 / 0  # Crash for now
-
-
 if __name__.split(".")[-1] == "__main__":
     # I'm ran as a module
     # drop low-level logging handler
     # set up logging properly
     del logging.root.handlers[:]
     logging.basicConfig(level=logging.DEBUG, handlers=[nslog.handler()])
-    import sys
-    logging.warn("%s", sys.path)
-    logging.warn("%s", __file__)
-    logging.debug("yep, I'm ran as a module")
+
+    # Interesting code goes here
     1 / 0
