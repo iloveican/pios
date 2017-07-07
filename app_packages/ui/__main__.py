98428670c8d242186d85dea5eb6fbb95e8df072c
Dima Tisnek <dimaqq@gmail.com>
2017-07-07 21:55:26 +0200

    Checkpoint

--- app_packages/ui/__main__.py
+++ app_packages/ui/__main__.py
@@ -1,6 +1,9 @@
 import nslog
 import logging
 
+from rubicon.objc import objc_method, ObjCClass
+from rubicon.objc.types import CGSize, CGRect, CGPoint
+
 
 if __name__.split(".")[-1] == "__main__":
     # I'm ran as a module
@@ -9,5 +12,45 @@ if __name__.split(".")[-1] == "__main__":
     del logging.root.handlers[:]
     logging.basicConfig(level=logging.DEBUG, handlers=[nslog.handler()])
 
-    # Interesting code goes here
-    1 / 0
+
+UIView = ObjCClass("UIView")
+UILabel = ObjCClass("UILabel")
+UIColor = ObjCClass("UIColor")
+UIWindow = ObjCClass("UIWindow")
+UIScreen = ObjCClass("UIScreen")
+UIResponder = ObjCClass("UIResponder")
+
+
+# define PythonAppDelegate
+
+# implement the protocol:
+# * applicationDidBecomeActive (to test)
+# * application:didFinishLaunchingWithOptions: (where you create window and view)
+# * application:didChangeStatusBarOrientation: (test screen orientation)
+
+# Resources:
+# https://oleb.net/blog/2012/02/app-launch-sequence-ios-revisited/
+# https://stackoverflow.com/questions/7520971/applications-are-expected-to-have-a-root-view-controller-at-the-end-of-applicati
+
+# Figure out window size
+# Create a window
+# Create a view controller
+# Set window's root view controller
+# Create a view
+# Set controller's view
+# display something in the view (to see that it works)
+
+
+def rect(x, y, w, h):
+    """ A la CGRectMake """
+    return CGRect(CGPoint(x, y), CGSize(w, h))
+
+
+class PythonAppDelegate(UIResponder):
+    # def __init__(self): init is not ran, as this is instantiated by ObjC runtime
+
+    @objc_method
+    def applicationDidBecomeActive(self) -> None:
+        logging.debug("became active")
+
+    # your code here
