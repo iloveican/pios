1380557f113777d1b55707a57efdc3df4c2b6e3b
Dima Tisnek <dimaqq@gmail.com>
2017-10-29 16:11:09 +0800

    New games and xcode settings

--- app_packages/game/__init__.py
+++ app_packages/game/__init__.py
@@ -11,10 +11,20 @@ braille = [l.split() for l in """
 ⓪ ① ② ③ ④ ⑤ ⑥ ⑦ ⑧ ⑨
 """.strip().splitlines()]
 
+images = [l.split() for l in """
+cat dog owl beach wave tree cup school
+😸 🐶 🦉 🏖 🌊 🌲 🍵 🏫
+""".strip().splitlines()]
+
+flags = [l.split() for l in """
+singapore malaysia usa finland japan korea italy china
+🇸🇬 🇲🇾 🇺🇸 🇫🇮 🇯🇵 🇰🇷 🇮🇹 🇨🇳
+""".strip().splitlines()]
+
 
 def get_tiles(size):
     assert not size % 2
-    source = random.choice((numbers, braille))
+    source = random.choice((flags,))
     game = random.sample(list(range(len(source[0]))), size // 2)
     tiles = sum(([(i, source[0][i]), (i, source[1][i])] for i in game), [])
     random.shuffle(tiles)
